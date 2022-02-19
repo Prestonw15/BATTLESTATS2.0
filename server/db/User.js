556e1775a8bf32b074b3501488eb3edec61675c8
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-19 11:04:08 -0700

    added the info to check the password as well the the saltrounds to crypt it

--- server/db/User.js
+++ server/db/User.js
@@ -4,20 +4,44 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true,
+    trim: true
   },
   email: {
     type: String,
-    isRequired: true,
-    isUnique: true,
+    required: true,
+    unique: true,
     match: [/.+@.+\..+/, 'Must match an email address!']
 },
 password: {
   type: String,
-  isRequired: true,
+  required: true,
   minLength: 8
 },
   friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+},
+{
+  toJSON: {
+    virtuals: true
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function(password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
 });
 
 
