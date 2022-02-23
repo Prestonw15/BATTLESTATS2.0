760be8610051555e6bb7dcee457d3f5094e2b481
Kyleah Justice <kikij432@gmail.com>
2022-02-22 20:13:47 -0700

    created the framework for the Steam login page, fixed a spelling error for assets

--- client/src/pages/steam.jsx
+++ client/src/pages/steam.jsx
@@ -3,3 +3,34 @@ import { Redirect } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+
+const Steam = () => {
+        return ( <>
+    <div classname='container'>
+        <Navbar/>
+        
+        <section class="hero" id="hero">
+            <img src="../assets/images/steam-icon.svg" alt="steam logo" class="cards" />
+             <p class="tagline">one site for all of your gaming stats&nbsp; &nbsp;</p>
+         </section>
+        <h3>New Users, Please enter you steam username</h3>
+        
+        <div class="form" id="login">
+            <form>
+                <label for="Username">Enter Steam username</label>
+                <input type="text" placeholder="Steam User" name="username" id="Username" class="login-input"/>
+                    <p id="error"></p>
+                    <button id="loginBtn" class="login-input" type="submit">Login</button>
+            </form>
+        </div>
+        
+    </div>
+    
+    <Footer/>
+            </>
+            
+    );
+};
+
+
+export default Steam;
