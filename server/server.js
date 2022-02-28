5d3a18d94e8c0de53f0887f90eb7f9225033e6f5
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-27 22:25:42 -0700

    PRESTON was a dipshit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ authenticate('prestonwatson.15@gmail.com', '')
 	.catch(console.error);
 
 const app = express();
-const PORT = procee.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({ 
   typeDefs, 
